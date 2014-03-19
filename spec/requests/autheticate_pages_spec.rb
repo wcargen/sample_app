54e846c55a6f929184ad7ee7e3eea0c208e0c31b
wcargen <wcargen@gmail.com>
2014-03-18 21:41:55 -0700

    Finish sign in

--- spec/requests/autheticate_pages_spec.rb
+++ spec/requests/autheticate_pages_spec.rb
@@ -5,7 +5,42 @@ describe "Authentication" do
   subject { page }
 
   describe "signin page" do
+    before { visit signin_path }
+
     it { should have_selector('h1', :text => 'Sign in') }
     it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_error_message('Invalid') } # defined in utilities.rb
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_error_message('Invalid') }
+      end
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before { valid_signin(user) } # valid_signin defined in #utilities.rb
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
   end
 end
